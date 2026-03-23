6402243060689632c3e13754d2ab8cdc8e612aca
Conor worthington <45695214+ConorWorthington@users.noreply.github.com>
2026-03-23 18:06:06 +0000

    Update single_feature_array_standard_scale to sample

--- src/kamae/spark/estimators/single_feature_array_standard_scale.py
+++ src/kamae/spark/estimators/single_feature_array_standard_scale.py
@@ -20,7 +20,7 @@ from pyspark import keyword_only
 from pyspark.sql import DataFrame
 from pyspark.sql.types import ArrayType, DataType, DoubleType, FloatType
 
-from kamae.spark.params import MaskValueParams, SingleInputSingleOutputParams
+from kamae.spark.params import MaskValueParams, SampleFractionParams, SingleInputSingleOutputParams
 from kamae.spark.transformers import StandardScaleTransformer
 from kamae.spark.utils import flatten_nested_arrays
 
@@ -29,6 +29,7 @@ from .base import BaseEstimator
 
 class SingleFeatureArrayStandardScaleEstimator(
     BaseEstimator,
+    SampleFractionParams,
     SingleInputSingleOutputParams,
     MaskValueParams,
 ):
